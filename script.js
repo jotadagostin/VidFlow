d02430205489e049b8cf6d54b17ba65f9118e13f
João Paulo Dagostin <dagostinjota@gmail.com>
2023-12-17 18:02:48 -0300

    Update script.js
    getting a better code with forEach

--- script.js
+++ script.js
@@ -30,21 +30,33 @@ barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
 function filtrarPesquisa() {
   const videos = document.querySelectorAll(".videos__item");
+  const valorFiltro = barraDePesquisa.value.toLocaleLowerCase();
 
-  if (barraDePesquisa.value != "") {
-    for (let video of videos) {
-      let titulo = video
-        .querySelector(".titulo-video")
-        .textContent.toLowerCase();
-      let valorFiltro = barraDePesquisa.value.toLowerCase();
+  videos.forEach((video) => {
+    const titulo = video
+      .querySelector(".titulo-video")
+      .textContent.toLocaleLowerCase();
+    video.style.display = valorFiltro
+      ? titulo.includes(valorFiltro)
+        ? "block"
+        : "none"
+      : "block";
+  });
 
-      if (!titulo.includes(valorFiltro)) {
-        video.style.display = "none";
-      } else {
-        video.style.display = "block";
-      }
-    }
-  } else {
-    video.style.display = "block";
-  }
+  // if (barraDePesquisa.value != "") {
+  //   for (let video of videos) {
+  //     let titulo = video
+  //       .querySelector(".titulo-video")
+  //       .textContent.toLowerCase();
+  //     let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+  //     if (!titulo.includes(valorFiltro)) {
+  //       video.style.display = "none";
+  //     } else {
+  //       video.style.display = "block";
+  //     }
+  //   }
+  // } else {
+  //   video.style.display = "block";
+  // }
 }
